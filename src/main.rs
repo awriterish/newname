d4bd4ed1e541edf64d1f011f26f5b9143bb08cae
Nova Marques <awriterish@outlook.com>
2021-01-31 15:53:25 -0600

    Working command
    Working implementation of the unix rename command in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::env;
+use std::fs;
 
 
 fn main() {
-    println!("Hello, world!");
+    let arg = env::args()
+        .skip(1)
+        .collect::<Vec<String>>();
+    if arg.iter().count() == 2 {
+        let _result = fs::rename(arg[0].clone(), arg[1].clone());
+        match _result {
+            Ok(_) => {println!("File renamed")}
+            Err(_) => {println!("Error in renaming file")}
+        }
+    } else {
+        println!("Incorrect number of arguments passed")
+    }
 }
